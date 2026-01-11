ac08268d048d3f16f88edc3a0bda172079cd4575
Hans-Peter Bergner <hapebe@gmx.de>
2026-01-11 21:53:33 +0100

    helloworld: start server directly from script

--- fastapi101/helloworld.py
+++ fastapi101/helloworld.py
@@ -5,3 +5,8 @@ app = FastAPI()
 @app.get("/")
 async def index():
     return {"message": "Hallo Welt!"}
+
+# Run the application with Uvicorn when this script is executed directly.
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
